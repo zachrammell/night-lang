8240a1e8cfb665e36e304374e97ba2f35ceeafd6
Zach Rammell <zrammell13@gmail.com>
2020-07-18 20:00:06 -0700

    added basic code generator
    it works although it uses the stack since without IR can't exactly do register allocation

--- src/night.cpp
+++ src/night.cpp
@@ -203,32 +203,32 @@ std::deque<token> tokenize_chunk(std::string const& chunk)
 
     unless (keyword.empty())
     {
-      std::cout << "found a keyword: " << keyword << std::endl;
+      //std::cout << "found a keyword: " << keyword << std::endl;
       tokens.emplace_back(create_token_keyword(keyword));
     }
     else unless (identifier.empty())
     {
-      std::cout << "found an identifier: " << identifier << std::endl;
+      //std::cout << "found an identifier: " << identifier << std::endl;
       tokens.emplace_back(create_token_identifier(identifier));
     }
     else unless (constant.empty())
     {
-      std::cout << "found a constant: " << constant << std::endl;
+      //std::cout << "found a constant: " << constant << std::endl;
       tokens.emplace_back(create_token_constant(std::stoi(constant)));
     }
     else unless (op_2.empty())
     {
-      std::cout << "found a 2-char operator: " << op_2 << std::endl;
+      //std::cout << "found a 2-char operator: " << op_2 << std::endl;
       tokens.emplace_back(create_token_operation(op_2));
     }
     else unless (op_1.empty())
     {
-      std::cout << "found a 1-char operator: " << op_1 << std::endl;
+      //std::cout << "found a 1-char operator: " << op_1 << std::endl;
       tokens.emplace_back(create_token_operation(op_1));
     }
     else unless (punc.empty())
     {
-      std::cout << "found punctuation: " << punc << std::endl;
+      //std::cout << "found punctuation: " << punc << std::endl;
       tokens.emplace_back(create_token_punctuation(punc[0]));
     }
   }
@@ -674,6 +674,140 @@ program* parse_program(std::deque<token>& tokens)
   return p;
 }
 
+// todo: once IR is a thing, remove these generate_ functions.
+// these are really just here so that the "vertical slice" can stay intact
+
+std::ostream& generate_expression(std::ostream& o, expression_node const& e)
+{
+  switch (e.m_type)
+  {
+  case expression_node::expr_type::binary_op:
+    if (e.m_binop.m_op_binary == operation_binary::add
+        || e.m_binop.m_op_binary == operation_binary::multiply)
+    {
+      // generate_expression will output code that puts the result in rax
+      generate_expression(o, *(e.m_binop.m_lhs));
+      o << "push rax\n";
+      // the rhs will end up in rax
+      generate_expression(o, *(e.m_binop.m_rhs));
+      // put the lhs in rcx
+      o << "pop rcx\n";
+      // LHS: RCX
+      // RHS: RAX
+    }
+    if (e.m_binop.m_op_binary == operation_binary::subtract
+        || e.m_binop.m_op_binary == operation_binary::divide)
+    {
+      // generate_expression will output code that puts the result in rax
+      generate_expression(o, *(e.m_binop.m_lhs));
+      o << "push rax\n";
+      // the rhs will end up in rax
+      generate_expression(o, *(e.m_binop.m_rhs));
+      // put the rhs in rcx
+      o << "mov rcx, rax\n";
+      // put the lhs in rax
+      o << "pop rax\n";
+      // LHS: RAX
+      // RHS: RCX
+    }
+    // operate on rax and rcx, with result in rax.
+    switch (e.m_binop.m_op_binary)
+    {
+    case operation_binary::add:
+      // lhs = 
+      o << "add rax, rcx\n";
+      break;
+    case operation_binary::subtract:
+      // lhs = lhs - rhs
+      o << "sub rax, rcx\n";
+      break;
+    case operation_binary::multiply:
+      o << "imul rcx\n";
+      break;
+    case operation_binary::divide:
+      // rax / rcx
+      o << "xor rdx, rdx\n";
+      o << "idiv rcx\n";
+      break;
+    }
+    break;
+  case expression_node::expr_type::unary_op:
+    generate_expression(o, *(e.m_unop.m_single));
+    // this can always operate on rax
+    switch (e.m_unop.op_unary)
+    {
+    case operation_unary::negate:
+      o << "neg rax\n";
+      break;
+    case operation_unary::ones_complement:
+      o << "not rax\n";
+      break;
+    case operation_unary::boolean_negate:
+      o << "test eax, eax\n"; // test if rax is 0
+      o << "xor rax, rax\n";  // zero out rax
+      o << "setz al\n";       // set lowest byte of rax to 1 if it was 0 
+      break;
+    }
+    break;
+  case expression_node::expr_type::value:
+    o << "mov rax, " << e.m_atom.m_value << "\n";
+    break;
+  }
+  return o;
+}
+
+std::ostream& generate_statement(std::ostream& o, statement_node const& s)
+{
+  generate_expression(o, *(s.m_return_value));
+  o << "ret\n";
+  return o;
+}
+
+std::ostream& generate_block(std::ostream& o, block_node const& b)
+{
+  for (auto const statement : b.m_statements)
+  {
+    generate_statement(o, *statement);
+  }
+  return o;
+}
+
+std::ostream& generate_function(std::ostream& o, function_node const& f)
+{
+  o << f.m_name << ":\n";
+  generate_block(o, *(f.m_block));
+  // todo: replaceable entry point name
+  if (f.m_name == "main")
+  {
+    //o << "mov rcx, 0\n" << "call _ExitProcess@4\n";
+  }
+  return o;
+}
+
+std::ostream& generate_declaration(std::ostream& o, declaration_node const& d)
+{
+  // if (is_function)
+  return generate_function(o, *(d.m_function));
+}
+
+std::ostream& generate_program(std::ostream& o, program const& p)
+{
+  for (auto const decl : p.m_declarations)
+  {
+    o << "global " << decl->m_function->m_name << "\n";
+  }
+
+  //o << "extern _ExitProcess@4\n";
+
+  o << "section .text\n"; 
+  for (auto const decl : p.m_declarations)
+  {
+    generate_declaration(o, *decl);
+  }
+
+  return o;
+}
+
 int main(int argc, char** argv)
 {
   if (argc == 1)
@@ -716,6 +850,8 @@ int main(int argc, char** argv)
 
   // generate assembly output
   std::ofstream out_assembly{ "asm.s" };
+  generate_program(out_assembly, *p);
+  generate_program(std::cout, *p);
 
   // todo: generic assembly interface for output to NASM or JIT
 
